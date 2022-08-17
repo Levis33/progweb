c55ecfaf3f9e30449ac2409b8b755a9907b9e9e4
Levis33 <39637793+Levis33@users.noreply.github.com>
2022-08-17 18:42:45 -0300

    iniciando o projeto django

---
+++ trabalho2/ecommerce/ecommerce/project/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Usuario(AbstractUser):
+    username = None
+    admin = models.ForeignKey('Usuario', null=True, blank=True, on_delete=models.CASCADE, db_column='fk_admin_id')
+    email = models.EmailField('email_address', unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
